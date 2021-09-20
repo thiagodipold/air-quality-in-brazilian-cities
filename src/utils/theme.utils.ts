023bb7494d6b67784d1ae27fa7874ede19e8766c
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-09-19 23:45:30 -0300

    novos estilos no tema

--- src/utils/theme.utils.ts
+++ src/utils/theme.utils.ts
@@ -3,5 +3,14 @@ import { createTheme } from "@material-ui/core";
 export const theme = createTheme({
   typography: {
     fontFamily: "Ubuntu",
+    body2: {
+      fontSize: "17px",
+      fontWeight: 400,
+    },
+  },
+  palette: {
+    info: {
+      main: "#fff",
+    },
   },
 });

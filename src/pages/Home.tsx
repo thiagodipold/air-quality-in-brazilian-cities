7a62d8fdeacc4e677663760a80d40205f9e5c676
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-09-19 23:46:18 -0300

    pagina da Home mockada com um exemplo de CityCard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,27 @@
 import { Grid, Typography } from "@material-ui/core";
+import CityCard from "../components/CityCard";
 
 const Home = () => {
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Typography gutterBottom color="#44c287" variant="h4" component="h1">
-          ar da minha cidade
-        </Typography>
-        <Typography paragraph variant="body1">
-          Clique em alguma cidade para ver mais detalhes sobre a qualidade do ar
-          e seu impacto na saúde.
-        </Typography>
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography gutterBottom color="#44c287" variant="h4" component="h1">
+            ar da minha cidade
+          </Typography>
+          <Typography paragraph variant="body1">
+            Clique em alguma cidade para ver mais detalhes sobre a qualidade do
+            ar e seu impacto na saúde.
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+
+      <Grid container>
+        <Grid item xs={12}>
+          <CityCard city="São Paulo" airQuality={100} />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 

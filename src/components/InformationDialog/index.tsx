c4cfd52a9d1a8890d588e700020f183590fa4a97
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-10-04 11:01:35 -0300

    adição de texto de qualidade do ar do nivel 'Boa'

--- src/components/InformationDialog/index.tsx
+++ src/components/InformationDialog/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import {
   AppBar,
   Box,
@@ -9,9 +11,9 @@ import {
   Typography,
 } from "@material-ui/core";
 import { TransitionProps } from "@material-ui/core/transitions";
-import * as React from "react";
-
 import { Close } from "@material-ui/icons";
+
+import { airQualityTexts } from "../../constants/city";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -30,17 +32,6 @@ interface IInformationDialog {
   airQuality: number;
   appBarBackground: string;
 }
-
-const airQualityTexts = {
-  Boa: "Lorem ipsum",
-  Moderada:
-    "Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar sintomas como tosse seca e cansaço. A população, em geral, não é afetada.",
-  Ruim: "Toda a população pode apresentar sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta. Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar efeitos mais sérios na saúde.",
-  "Muito Ruim":
-    "Toda a população pode apresentar agravamento dos sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta e ainda falta de ar e respiração ofegante. Efeitos ainda mais graves à saúde de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas).",
-  Péssima:
-    "Toda a população pode apresentar sérios riscos de manifestações de doenças respiratórias e cardiovasculares. Aumento de mortes prematuras em pessoas de grupos sensíveis.",
-};
 
 const InformationDialog: React.FC<IInformationDialog> = ({
   open,
--- src/constants/city.ts
+++ src/constants/city.ts
@@ -16,3 +16,14 @@ export const cities = [
   "sao-paulo",
   "taubate",
 ];
+
+export const airQualityTexts = {
+  Boa: "A qualidade do ar é considerada satisfatória, a poluição do ar representa pouco ou nenhum risco.",
+  Moderada:
+    "Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar sintomas como tosse seca e cansaço. A população, em geral, não é afetada.",
+  Ruim: "Toda a população pode apresentar sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta. Pessoas de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas) podem apresentar efeitos mais sérios na saúde.",
+  "Muito Ruim":
+    "Toda a população pode apresentar agravamento dos sintomas como tosse seca, cansaço, ardor nos olhos, nariz e garganta e ainda falta de ar e respiração ofegante. Efeitos ainda mais graves à saúde de grupos sensíveis (crianças, idosos e pessoas com doenças respiratórias e cardíacas).",
+  Péssima:
+    "Toda a população pode apresentar sérios riscos de manifestações de doenças respiratórias e cardiovasculares. Aumento de mortes prematuras em pessoas de grupos sensíveis.",
+};
